93721ed1ed003e3a455540e4b8cd4f94c7841a9c
BlackWerf1257 <mklsjh@naver.com>
2024-05-07 18:37:09 +0900

    아이템 배치 코드 업데이트

--- Assets/Script/DIY/Seed_Change.cs
+++ Assets/Script/DIY/Seed_Change.cs
@@ -1,9 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
+
 
 public class Seed_Change : MonoBehaviour
 {
+    [Serializable]
+    public struct PlayerStats
+    {
+        [SerializeField] int movementSpeed;
+        public int hitPoints;
+        public bool hasHealthPotion;
+    }
+
+    //Use [SerializeField] attribute to ensure that the private field is serialized
+    
+    private PlayerStats stats;
+    
     [SerializeField] private int seed;
     
     // Start is called before the first frame update
--- Assets/Script/Seed/Generator.cs
+++ Assets/Script/Seed/Generator.cs
@@ -8,7 +8,7 @@ public class Generator : MonoBehaviour
 {
     [SerializeField] Transform parentObj;
     private List<Transform> floorObj;
-    [SerializeField] public List<GameObject> wallObj;
+    [SerializeField] public List<GameObject> enemyObj;
     [SerializeField] public List<GameObject> trapObj;
     [SerializeField] public List<GameObject> itemObj;
 
@@ -22,55 +22,20 @@ public class Generator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        MapSetting(trapObj, propCount);
+        //Prop
+        MapSetting(itemObj, propCount);
+        //Trap
+        MapSetting(trapObj, trapCount);
+        //Enemy
+        MapSetting(enemyObj, enemyCount);
         
-        /*for(int i = 0; i < GroundObjectCount; i++)
-        {
-            Vector3 pos = Random.insideUnitSphere * 7;
-            pos.y = 0;
-            GameObject selected = GroundObjects[Random.Range(0, GroundObjects.Length)];
-            
-            GameObject ground = Instantiate(selected, pos, selected.transform.rotation);
-            ground.transform.rotation = Quaternion.Euler(-90, Random.Range(0, 360), 0);
-        }
-        for (int i = 0; i < RockObjectCount; i++)
-        {
-            Vector3 pos = Random.insideUnitSphere * 7;
-            pos.y = 20;
-            RaycastHit hit;
-            if (Physics.Raycast(pos, Vector3.down, out hit))
-            {
-                GameObject selected = RockObjects[Random.Range(0, RockObjects.Length)];
-
-                GameObject ground = Instantiate(selected, hit.point, selected.transform.rotation);
-                ground.transform.rotation = Quaternion.Euler(-90, Random.Range(0, 360), 0);
-            }
-        }
-        for (int i = 0; i < TreeObjectCount; i++)
-        {
-            Vector3 pos = Random.insideUnitSphere * 7;
-            pos.y = 20;
-            RaycastHit hit;
-            if (Physics.Raycast(pos, Vector3.down, out hit))
-            {
-                GameObject selected = TreeObjects[Random.Range(0, TreeObjects.Length)];
-
-                GameObject ground = Instantiate(selected, hit.point, selected.transform.rotation);
-                ground.transform.rotation = Quaternion.Euler(-90, Random.Range(0, 360), 0);
-            }
-        }*/
-
     }
 
     void MapSetting(List<GameObject> obj, int objCnt)
     {
-        
-        
         for (int i = 0; i < objCnt; i++)
         {
-            Vector3 pos;
-
-                pos = Random.insideUnitSphere * mapSize;
+            Vector3 pos = Random.insideUnitSphere * mapSize;
 
                 if (objTrans.Count != 0)
                 {
@@ -91,31 +56,14 @@ public class Generator : MonoBehaviour
             ground.transform.localScale = Vector3.one;
             ground.transform.parent = parentObj;
             ground.transform.rotation = Quaternion.Euler(0, Random.Range(0, 360), 0);
-            ground.AddComponent<Rigidbody>();
-            
-            if (ground.transform.childCount == 0)
-            {
-                ground.AddComponent<BoxCollider>();
-                StartCoroutine(DestoryRigidbody(ground.GetComponent<Rigidbody>(), ground.GetComponent<BoxCollider>()));
-            }
-            else
-            {
-                ground.transform.GetChild(1).gameObject.AddComponent<BoxCollider>();
-                StartCoroutine(DestoryRigidbody(ground.GetComponent<Rigidbody>(), ground.transform.GetChild(1).
-                    gameObject.GetComponent<BoxCollider>()));
-            }
-            
-            
+          
+            StartCoroutine(DestoryRigidbody(ground.GetComponent<Rigidbody>()));
         }
 
-        IEnumerator DestoryRigidbody(Rigidbody rigid, BoxCollider collider)
+        IEnumerator DestoryRigidbody(Rigidbody rigid)
         {
             yield return new WaitForSeconds(.5f);
-            //Destroy(collider);
-            //Destroy(rigid);
+            Destroy(rigid);
         }
     }
-        //Prop
-        //Trap
-        //Enemy
 }
